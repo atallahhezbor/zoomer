1af2022fb2faf15b0a0d4df4111f1509ea7a5474
Atallah <atallahhezbor@gmail.com>
2016-04-20 09:58:24 -0400

    Initial commit. Adds core functionality

---
+++ README.md
@@ -0,0 +1,3 @@
+# Eager Font Size Control
+
+This Eager app allows a website visitor to change the font size of a page they are visiting without affecting the size and layout of elements on the page.
---
+++ app.js
@@ -0,0 +1,68 @@
+(function(){
+
+  // We check for features which are not universally supported, and don't try to
+  // show the app if it would error.
+  if (!window.addEventListener)
+    return;
+
+  // The INSTALL_OPTIONS constant is inserted by the Eager bundler.  It's value is the
+  // value of the options defined in the install.json file.
+  var options = INSTALL_OPTIONS;
+
+  var zoomLevel = 1;
+  var el = null;
+
+  var decreaseFont = function() {
+    zoomLevel -= 0.1;
+    document.documentElement.style.fontSize=zoomLevel + 'em';
+    update();
+  }
+
+  var increaseFont = function() {
+    zoomLevel += 0.1;
+    document.documentElement.style.fontSize=zoomLevel + 'em';
+    update();
+  }
+
+  var updateElement = function(){
+    // We pass in the last element to allow us to restore the removed element
+    // when we do live updating of the preview.  Details:
+    // https://eager.io/developer/docs/install-json/preview#dealing-with-element-fields
+    el = Eager.createElement(options.element, el);
+    el.className = 'zoom-widget';
+  };
+
+  var update = function(){
+    updateElement();
+
+    el.innerHTML = '<div><button id="decrease">   -   </button>' + zoomLevel + '<button id="increase">  +     </button></div>';
+      document.getElementById('decrease').addEventListener('click', handler = function(){
+        decreaseFont();
+      });
+      document.getElementById('increase').addEventListener('click', handler = function(){
+        increaseFont();
+      });
+  }
+
+  var setOptions = function(opts){
+    options = opts;
+
+    update();
+  }
+
+
+
+  // Since we're adding an element to the body, we need to wait until the DOM is
+  // ready before inserting our widget.
+  if (document.readyState == 'loading')
+    document.addEventListener('DOMContentLoaded', update);
+  else
+    update();
+
+  // This is used by the preview to enable live updating of the app while previewing.
+  // See the preview.handlers section of the install.json file to see where it's used.
+  INSTALL_SCOPE = {
+    setOptions: setOptions
+  };
+
+})()
